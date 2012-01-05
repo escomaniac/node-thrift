d4f10a15fbdf54e4f51c47ec36f8ffa75658fd24
Jim Klucar <jklucar@texeltek.com>
2012-01-04 21:23:28 -0500

    not sure that collection writes are working correctly

--- test/compact.test.js
+++ test/compact.test.js
@@ -4,13 +4,12 @@
 *
 */
 
-
-
-
 var tcompact = require('../lib/thrift/protocol/protocol_compact').TCompactProtocol;
 
 var cp = new tcompact();
 
-cp.writeVarint(300);
+cp.writeVarint(1);   // 1 
+cp.writeVarint(300); // output is 44034d 0xAC02
+
 
 
